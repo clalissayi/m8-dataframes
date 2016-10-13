859418fc1458e928dcc0d5c40232c6c5f841354b
Clalissa <cmy24@uw.edu>
2016-10-13 14:59:45 -0700

    class exercise

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,37 +1,57 @@
 # Exercise 6: Popular Baby Names Over Time
 
 # Read in the female baby names csv file into a variable called `female.names`
-
+setwd("~/Desktop/INFO 201/m8-dataframes/exercise-6")
+female.names <- read.csv("~/Desktop/INFO 201/m8-dataframes/exercise-6/data/female_names.csv", 
+                         stringsAsFactors=FALSE)
 
 # Create a vector `year` as the year column of the dataset
-
+year <- female.names$year
 
 # Create a vector `name` as the name column of the datset
 # As in the last exercise, you'll need to convert this column to a vector
-
+name <- female.names$name
 
 # Create a vector `prop` as the proportion column of the dataset
-
+prop <- female.names$prop
 
 # Create a vector `names.2013` as your name vector where your year vector is 2013
-
+names.2013 <- name[year==2013]
 
 # Create a vector `prop.2013` as the your prop vector where your year vecctor is 2013
-
+prop.2013 <- prop[year==2013]
 
 # What was the most popular female name in 2013?
+popular.name <- names.2013[prop.2013==max(prop.2013)]
 
 
 # Write a funciton `MostPopular` that takes in a value `my.year`, and returns
 # a sentence stating the most popular name in that year. 
 
+MostPopular <- function(my.year){
+  names.x <- name[year==my.year]
+  prop.x <- prop[year == my.year]
+  popular.name <- names.x[prop.x==max(prop.x)]
+  return(popular.name)
+}
+
 
 # What was the most popular female name in 1994?
-
+MostPopular(1994)
 
 ### Bonus ###
 
 # Write a function `HowPopular` that takes in a name and a year, and returns
 # a sentence with how popular that name was in that year
+
+HowPopular <- function(name, my.year){
+  names.x <- name[year==my.year]
+  prop.x <- prop[year == my.year]
+  popular.name <- names.x[prop.x==max(prop.x)]
+  
+  return(popular.name, )
+}
+
+
 
 # How popular was the name 'Laura' in 1995
